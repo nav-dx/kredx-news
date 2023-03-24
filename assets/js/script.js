8be17754ae08271549771e8221e28715c173f2f9
Naveen <naveen.m@kredx.com>
2023-03-24 14:23:38 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+let data = {
+  kredx: {
+    title: "KredX",
+    news: [
+      {
+        id: 1,
+        imgUrl: "The Print",
+        desc: "KredX unveils new brand identity; positions itself as one-stop solution provider of supply chain financing",
+        redirect_url: "https://www.kredx.com/",
+      },
+      {
+        id: 2,
+        imgUrl: "ANI",
+        desc: "KredX unveils new brand identity; positions itself as one-stop solution provider of supply chain financing",
+        redirect_url: "https://www.kredx.com/",
+      },
+      {
+        id: 3,
+        imgUrl: "Upstream Life",
+        desc: "Video: Alternate lending powered by technology can make SMBs prosper in India: the story of KredX",
+        redirect_url: "https://www.kredx.com/",
+      },
+      {
+        id: 4,
+        imgUrl: "Financial Express",
+        desc: "KredX unveils new brand identity",
+        redirect_url: "https://www.kredx.com/",
+      },
+      {
+        id: 5,
+        imgUrl: "Financial Express",
+        desc: "Video: TReDS brought the power of financing to MSMEs instead of banks earlier, says KredX’s Manish Kumar",
+        redirect_url: "https://www.kredx.com/",
+      },
+      {
+        id: 6,
+        imgUrl: "ANI",
+        desc: "Video: TReDS brought the power of financing to MSMEs instead of banks earlier, says KredX’s Manish Kumar",
+        redirect_url: "https://www.kredx.com/",
+      },
+    ],
+  },
+};
